acb480d1e21c5d767f89eed17a27e33703f1d6a5
신희성 <vvwv3092@gmail.com>
2024-03-05 17:45:04 +0900

    Add use jpa application

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,40 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+
+            findMember.setName("USER_JPA");
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(5)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+//            em.persist(findMember);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
