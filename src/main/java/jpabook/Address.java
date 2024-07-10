3c021fce3b8d46a08743d49a0b72a1cacf6b4a91
신희성 <vvwv3092@gmail.com>
2024-07-10 11:06:23 +0900

    Add 값 타입 비교

--- src/main/java/jpabook/Address.java
+++ src/main/java/jpabook/Address.java
@@ -2,6 +2,8 @@ package jpabook;
 
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 // 임배다드 터압과 같은 값 타입은 여러 엔티티에서 공유하면 위험하다
 // 값 타입 객체의 인스턴스를 공유하는 것은 예상치 못한 사이드 이펙트가 발생할 수 있다.
 // 그렇지만 래퍼런스를 공유하는 행위를 막을 순 없으므로 값 변경을 막아 해결할 수 있다.
@@ -34,5 +36,19 @@ public class Address {
     }
 
     public Address() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address address)) return false;
+        return Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet()) &&
+                Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
 }
--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -19,14 +19,21 @@ public class JpaMain {
 
             Member member = new Member();
 
-            Address address = new Address("A", "B", "C");
+            Address address1 = new Address("A", "B", "C");
+            Address address2 = new Address("A", "B", "C");
             Period period = new Period(LocalDateTime.now(), LocalDateTime.now());
 
             member.setPeriod(period);
-            member.setHomeAddress(address);
-            member.setWorkAddress(address);
+            member.setHomeAddress(address1);
+            member.setWorkAddress(address1);
 
             em.persist(member);
+
+            // 동일성 비교: 인스턴스의 참조값 비교 ==, 동등성 비교 인스턴스의 값 비교 equals()
+            // 값 타입은 equals()로 동등성 비교를 해서 값이 동일한지 판별해야한다.
+            // -> 값 비교가 필요할때 임베디드 타입과 같은 객체 값 타입은 equals()를 재정의해서 동등성 비교를 가능하게 해야한다.
+
+            System.out.println(address1.equals(address2));
 
             tx.commit();
         } catch (Exception e) {
