10bc7531c3f34bc8096c97d3f44c369400fdf4ce
신희성 <vvwv3092@gmail.com>
2024-07-12 09:41:27 +0900

    Add 값 타입 컬렉션

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -5,6 +5,7 @@ import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,22 +19,35 @@ public class JpaMain {
         try {
 
             Member member = new Member();
-
-            Address address1 = new Address("A", "B", "C");
-            Address address2 = new Address("A", "B", "C");
-            Period period = new Period(LocalDateTime.now(), LocalDateTime.now());
-
-            member.setPeriod(period);
-            member.setHomeAddress(address1);
-            member.setWorkAddress(address1);
+            member.setName("ASd");
+            member.setAddressHistory(List.of(new Address("Asd", "ASd", "Asd"),
+                    new Address("Aㅇㄴㅁㅇㄴㅇsd", "ASd", "Asd")));
+            member.getFavoriteFoods().add("A");
+            member.getFavoriteFoods().add("B");
+            member.getFavoriteFoods().add("C");
 
             em.persist(member);
 
-            // 동일성 비교: 인스턴스의 참조값 비교 ==, 동등성 비교 인스턴스의 값 비교 equals()
-            // 값 타입은 equals()로 동등성 비교를 해서 값이 동일한지 판별해야한다.
-            // -> 값 비교가 필요할때 임베디드 타입과 같은 객체 값 타입은 equals()를 재정의해서 동등성 비교를 가능하게 해야한다.
+            em.flush();
+            em.clear();
 
-            System.out.println(address1.equals(address2));
+            System.out.println("------------");
+            Member findMember = em.find(Member.class, member.getId()); // 컬렉션 값 타입은 기본적으로 지연로딩이 됨
+            System.out.println("------------");
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println(favoriteFood);
+            }
+
+            // 값 타입을 수정하고 싶으면 각각의 값 타입은 불변해야하기 때문에 갈아끼워야함
+            // 하지만 값 타입은 추적하기 어렵다 (모든 컬럼을 PK로 잡아야함)
+            // 그렇기 때문에 컬렉션을 수정한다면... 연관되어있는 엔티티가 가지고 있는 모든 컬렉션을 삭제하고 현재 있는 모든 컬렉션 데이터를 다시 insert 한다.
+            // -> 결론. 쓰지 말자. OrderColumn으로 해결 가능하긴 하지만 이것도 예측 불가능하게 동작한다. 일대다 관계를 고려
+
+            // equals 로 비교해서 동일한 것이라면 삭제 (equals, hashcode를 잘 구현해두었기 때문)
+            findMember.getAddressHistory().remove(new Address("Asd", "ASd", "Asd"));
+            findMember.getAddressHistory().add(new Address("NEW", "ASd", "Asd"));
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabook/Member.java
+++ src/main/java/jpabook/Member.java
@@ -4,14 +4,46 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "member_id")
     private Long id;
     private String name;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "favorite_food", joinColumns =
+        @JoinColumn(name = "member_id")
+    )
+    @Column(name = "food_name")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "address", joinColumns =
+        @JoinColumn(name = "member_id")
+    )
+    private List<Address> addressHistory = new ArrayList<>();
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
 
     // 임베디드 타입은 그냥 값 타입일 뿐이다. 엔티티의 값! 엔티티와 라이프사이클이 같다.
     // 임베디드 타입을 사용하기 전과 후에 매핑하는 테이블은 같다.
@@ -20,25 +52,25 @@ public class Member {
     // 잘 설계한 ORM 애플리케이션은 매핑한 테이블 수보다 클래스의 수가 더 많다.
 
     // Period
-    @Embedded
-    private Period period;
-
-    // Address
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "city", column = @Column(name = "home_city")),
-            @AttributeOverride(name = "street", column = @Column(name = "home_street")),
-            @AttributeOverride(name = "zipcode", column = @Column(name = "home_zipcode")),
-    })
-    private Address homeAddress;
-
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "city", column = @Column(name = "work_city")),
-            @AttributeOverride(name = "street", column = @Column(name = "work_street")),
-            @AttributeOverride(name = "zipcode", column = @Column(name = "work_zipcode")),
-    })
-    private Address WorkAddress;
+//    @Embedded
+//    private Period period;
+//
+//    // Address
+//    @Embedded
+//    @AttributeOverrides({
+//            @AttributeOverride(name = "city", column = @Column(name = "home_city")),
+//            @AttributeOverride(name = "street", column = @Column(name = "home_street")),
+//            @AttributeOverride(name = "zipcode", column = @Column(name = "home_zipcode")),
+//    })
+//    private Address homeAddress;
+//
+//    @Embedded
+//    @AttributeOverrides({
+//            @AttributeOverride(name = "city", column = @Column(name = "work_city")),
+//            @AttributeOverride(name = "street", column = @Column(name = "work_street")),
+//            @AttributeOverride(name = "zipcode", column = @Column(name = "work_zipcode")),
+//    })
+//    private Address WorkAddress;
 
     public Long getId() {
         return id;
@@ -54,29 +86,5 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Period getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(Period period) {
-        this.period = period;
-    }
-
-    public Address getHomeAddress() {
-        return homeAddress;
-    }
-
-    public void setHomeAddress(Address homeAddress) {
-        this.homeAddress = homeAddress;
-    }
-
-    public Address getWorkAddress() {
-        return WorkAddress;
-    }
-
-    public void setWorkAddress(Address workAddress) {
-        WorkAddress = workAddress;
     }
 }
