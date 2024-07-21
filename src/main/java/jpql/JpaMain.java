dfacb9cd8c41017be9eb9be67688449a2e6634f0
신희성 <vvwv3092@gmail.com>
2024-07-21 19:23:46 +0900

    Add paging

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -16,34 +16,26 @@ public class JpaMain {
 
         try {
 
-            // 프로젝션 - select 절에 조회할 대상을 지정하는것
-            // 엔티티, 임베디드, 스칼라 타입이 프로젝션 대상이될 수 있음
-            Member member = new Member();
-            member.setUsername("123");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("123");
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-            // 엔티티 타입 프로젝션으로 엔티티를 조회해오면 영속성 컨텍스트에 관리된다.
-            List<Member> members1 = em.createQuery("select m from Member m", Member.class)
+            // JPA는 페이징을 setFirstResult, setMaxResults이 두 API로 추상화함
+            // 각각의 데이터베이스의 방언에 맞게 알아서 쿼리를 짜주어서 코드 상에서는 아래처럼 추상화된 API만 사용하면 됨
+
+            List<Member> members = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
                     .getResultList();
 
-            members1.get(0).setAge(100);
-
-            // 임베디드 타입 프로젝션
-            em.createQuery("select o.address from Order o", Address.class)
-                    .getResultList();
-
-            // 스칼라 타입 프로젝션
-            // Query 타입으로 조회(Object), Dto 객체로 바로 조회하기 (new 키워드 사용해서)
-            List<MemberDto> resultList = em.createQuery("select distinct new jpql.dto.MemberDto(m.username, m.age) from Member m", MemberDto.class)
-                    .getResultList();
-
-            for (MemberDto memberDto : resultList) {
-                System.out.println("memberDto.getUsername() = " + memberDto.getUsername());
-                System.out.println("memberDto.getAge() = " + memberDto.getAge());
+            for (Member member1 : members) {
+                System.out.println("member1 = " + member1);
             }
 
 
